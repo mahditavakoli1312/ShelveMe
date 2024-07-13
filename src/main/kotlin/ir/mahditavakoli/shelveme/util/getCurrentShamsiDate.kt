9e10e491330607afda7dad833803ca15ecfd97ad
Mahdi Tavakoli <mahditavakoli7878@gmail.com>
2024-07-13 10:22:09 +0330

    Version 1.0.1

--- src/main/kotlin/ir/mahditavakoli/shelveme/util/getCurrentShamsiDate.kt
+++ src/main/kotlin/ir/mahditavakoli/shelveme/util/getCurrentShamsiDate.kt
@@ -1,20 +1,16 @@
 package ir.mahditavakoli.shelveme.util
 
-import com.github.eloyzone.jalalicalendar.DateConverter
-import com.github.eloyzone.jalalicalendar.JalaliDateFormatter
-import java.text.SimpleDateFormat
-import java.util.*
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 fun getPersianCurrentDateYMD(): String {
-    val calendar: Calendar = Calendar.getInstance()
+    // Get the current date and time
+    val currentDateTime = LocalDateTime.now()
 
-    val year: Int = calendar.get(Calendar.YEAR)
-    val month: Int = calendar.get(Calendar.MONTH) + 1 // January is 0, so we add 1
-    val day: Int = calendar.get(Calendar.DAY_OF_MONTH)
-    val dateConverter = DateConverter()
-    val time = SimpleDateFormat("HH-mm-ss").format(Date())
+    // Define the formatter for the desired format
+    val formatter = DateTimeFormatter.ofPattern("'d-'yyyy-MM-dd-'t-'HH:mm")
 
-
-    return dateConverter.gregorianToJalali(year, month, day)
-        .format(JalaliDateFormatter("yyyy/mm/dd", JalaliDateFormatter.FORMAT_IN_ENGLISH)).replace("/", "-")+"-"+time
+    // Format the current date and time using the formatter
+    return currentDateTime.format(formatter)
 }
